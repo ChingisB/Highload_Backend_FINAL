27e78a87c739de2345e1c1cc527a570925184035
chingis <chingis.bagdatov@gmail.com>
2024-12-09 23:24:14 +0500

    add views

---
+++ final/main/urls.py
@@ -0,0 +1,27 @@
+from rest_framework.routers import DefaultRouter
+from .views import (
+    UserViewSet, 
+    ProductViewSet, 
+    OrderViewSet, 
+    CartItemViewSet,
+    OrderItemViewSet,
+    ShoppingCartViewSet, 
+    CategoryViewSet,
+    PaymentViewSet, 
+    ReviewViewSet, 
+    WishlistViewSet, 
+    WishlistItemViewSet
+)
+
+router = DefaultRouter()
+router.register('users', UserViewSet)
+router.register('categories', CategoryViewSet)
+router.register('products', ProductViewSet)
+router.register('orders', OrderViewSet)
+router.register('order-items', OrderItemViewSet)
+router.register('shopping-carts', ShoppingCartViewSet)
+router.register('cart-items', CartItemViewSet)
+router.register('payments', PaymentViewSet)
+router.register('reviews', ReviewViewSet)
+router.register('wishlists', WishlistViewSet)
+router.register('wishlist-items', WishlistItemViewSet)
--- final/main/views.py
+++ final/main/views.py
@@ -1,3 +1,83 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import (
+    User, 
+    Category, 
+    Product, 
+    Order, 
+    OrderItem, 
+    ShoppingCart, 
+    CartItem,
+    Payment,
+    Review,
+    Wishlist,
+    WishlistItem
+)
+from .serializers import (
+    UserSerializer, 
+    CategorySerializer, 
+    ProductSerializer,
+    OrderSerializer,
+    OrderItemSerializer, 
+    ShoppingCartSerializer, 
+    CartItemSerializer, 
+    PaymentSerializer, 
+    ReviewSerializer, 
+    WishlistSerializer, 
+    WishlistItemSerializer
+)
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
+
+
+class ShoppingCartViewSet(viewsets.ModelViewSet):
+    queryset = ShoppingCart.objects.all()
+    serializer_class = ShoppingCartSerializer
+
+
+class CartItemViewSet(viewsets.ModelViewSet):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class WishlistViewSet(viewsets.ModelViewSet):
+    queryset = Wishlist.objects.all()
+    serializer_class = WishlistSerializer
+
+
+class WishlistItemViewSet(viewsets.ModelViewSet):
+    queryset = WishlistItem.objects.all()
+    serializer_class = WishlistItemSerializer
